a910dce017149ff6326aea6d4976f4a9ffc4176a
Francisco Filho <francisco.guinarte@outlook.com.br>
2022-04-22 11:25:20 -0300

    feat: Adicionando regra de negocio para Venda

--- src/main/kotlin/com/curso/service/VendaService.kt
+++ src/main/kotlin/com/curso/service/VendaService.kt
@@ -1,16 +1,39 @@
 package com.curso.service
 
 import com.curso.dto.input.VendaInput
+import com.curso.dto.output.Parcela
+import com.curso.dto.output.Venda
 import com.curso.http.VeiculoHttp
 import jakarta.inject.Singleton
+import java.time.LocalDateTime
 
 @Singleton
-class VendaService (
+class VendaService(
     private val veiculoHttp: VeiculoHttp
-    ){
+) {
 
-    fun realizarVenda(vendaInput: VendaInput){
+    fun realizarVenda(vendaInput: VendaInput) {
         val veiculo = veiculoHttp.findById(vendaInput.veiculo)
-        println(veiculo.toString())
+        var parcelas = ArrayList<Parcela>()
+        val valorParcela = vendaInput.valor.divide(vendaInput.quantidadeParcelas.toBigDecimal())
+        var dataVencimento = LocalDateTime.now().plusMonths(1)
+
+        for (i in 1..vendaInput.quantidadeParcelas) {
+            var parcela: Parcela = Parcela(
+                valor = valorParcela,
+                dataVencimento = dataVencimento.toString()
+            )
+            parcelas.add(parcela)
+            dataVencimento = dataVencimento.plusMonths(1)
+
+        }
+
+        var venda = Venda(
+            veiculo = veiculo,
+            valor = vendaInput.valor,
+            cliente = vendaInput.cliente,
+            parcelas = parcelas
+        )
+        println(venda)
     }
 }
